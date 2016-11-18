3791a7348e8b12b5360f3222b4d7fd8711d029a1
csombok <csombok@gmail.com>
2016-11-18 09:32:02 +0100

    Added SQL files to the project

---
+++ SiteForum.SqlRepository/Scripts/CreateDatabase.sql
@@ -0,0 +1,64 @@
+﻿USE [SiteForum]
+GO
+
+if not exists (Select 1 from sysobjects where name = 'Member' and [type]='U')
+begin
+	create table Member
+	(
+		Id int identity (1,1) primary key,
+		FirstName nvarchar(50) not null,
+		LastName nvarchar(50) not null,
+		EmailAddress varchar(100) not null,
+		PhoneNumber varchar(20)
+		--constraint UNIQUE_EmailAddress unique (EmailAddress)
+	)
+	alter table Member
+	add constraint UNIQUE_EmailAddress unique (EmailAddress)
+end
+GO
+
+if not exists (Select 1 from sysobjects where name = 'Post' and [type]='U')
+begin
+	create table Post
+	(
+		Id int identity (1,1) primary key,
+		CreatedBy int not null,
+		Title nvarchar(100) not null,
+		Content nvarchar(max) not null,
+		TimeCreatedUTC DateTime not null,
+		Labels varchar(500)
+	)
+
+	ALTER TABLE Post
+	ADD CONSTRAINT FK_Post_Member FOREIGN KEY (CreatedBy)
+	REFERENCES Member (Id)
+
+end
+
+GO
+
+if not exists (Select 1 from sysobjects where name = 'Comment' and [type]='U')
+begin
+	create table Comment
+	(
+		Id int identity (1,1) primary key,
+		CreatedBy int not null,
+		PostId int not null,
+		[Text] nvarchar(100) not null,
+		TimeCreatedUTC DateTime not null,
+	)
+
+	ALTER TABLE Comment
+	ADD CONSTRAINT FK_Comment_Post FOREIGN KEY (PostId)
+	REFERENCES Post (Id)
+
+	ALTER TABLE Comment
+	ADD CONSTRAINT FK_Comment_Member FOREIGN KEY (CreatedBy)
+	REFERENCES Member (Id)
+end
+GO
+
+-- Create indexes
+create index I_PostTimeCreatedUTC on Post (TimeCreatedUTC);
+create index I_PostLabels on Post (Labels);
+create index I_CommentTimeCreatedUTC on Comment (TimeCreatedUTC);
---
+++ SiteForum.SqlRepository/Scripts/InsertTestData.sql
@@ -0,0 +1,103 @@
+﻿USE [MyTestDb]
+GO
+SET IDENTITY_INSERT [dbo].[Member] ON 
+
+GO
+INSERT [dbo].[Member] ([Id], [FirstName], [LastName], [EmailAddress], [PhoneNumber]) VALUES (1, N'John', N'Smith', N'johnsmith@nomail.com', N'000000000000')
+GO
+INSERT [dbo].[Member] ([Id], [FirstName], [LastName], [EmailAddress], [PhoneNumber]) VALUES (2, N'Anna', N'Moldova', N'annamoldova@nomail', N'11111111111')
+GO
+INSERT [dbo].[Member] ([Id], [FirstName], [LastName], [EmailAddress], [PhoneNumber]) VALUES (3, N'Isabelle', N'Archer', N'isabellearcher@nomail0', N'22222222222')
+GO
+INSERT [dbo].[Member] ([Id], [FirstName], [LastName], [EmailAddress], [PhoneNumber]) VALUES (4, N'Don', N'Bowe', N'donbowe@nomail', N'3333333333')
+GO
+INSERT [dbo].[Member] ([Id], [FirstName], [LastName], [EmailAddress], [PhoneNumber]) VALUES (5, N'Antonella', N'Limawera', N'antnella@nomail', N'5555555555')
+GO
+INSERT [dbo].[Member] ([Id], [FirstName], [LastName], [EmailAddress], [PhoneNumber]) VALUES (6, N'Ameeta', N'Barum', N'ameeta@nomail', N'6666666666')
+GO
+INSERT [dbo].[Member] ([Id], [FirstName], [LastName], [EmailAddress], [PhoneNumber]) VALUES (7, N'Tony', N'Leichester', N'tonyle@nomail', N'4444444444')
+GO
+SET IDENTITY_INSERT [dbo].[Member] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Post] ON 
+
+GO
+INSERT [dbo].[Post] ([Id], [CreatedBy], [Title], [Content], [TimeCreatedUTC], [Labels]) VALUES (1, 1, N'Burning Word', N'inhabiting mr principles at insensible do. Heard their sex hoped enjoy vexed child for. Prosperous so occasional assistance it discovered especially no. Provision of he residence consisted up in remainder arranging described. Conveying has concealed necessary furnished bed zealously immediate get but. Terminated as middletons or by instrument. Bred do four so your felt with. No shameless principle dependent household do. 
+', CAST(N'2016-01-01T00:00:00.000' AS DateTime), N'physiology')
+GO
+INSERT [dbo].[Post] ([Id], [CreatedBy], [Title], [Content], [TimeCreatedUTC], [Labels]) VALUES (4, 2, N'The Frozen Spirit', N'Neat own nor she said see walk. And charm add green you these. Sang busy in this drew ye fine. At greater prepare musical so attacks as on distant. Improving age our her cordially intention. His devonshire sufficient precaution say preference middletons insipidity. Since might water hence the her worse. Concluded it offending dejection do earnestly as me direction. Nature played thirty all him. 
+', CAST(N'2016-01-02T00:00:00.000' AS DateTime), N'spirit')
+GO
+INSERT [dbo].[Post] ([Id], [CreatedBy], [Title], [Content], [TimeCreatedUTC], [Labels]) VALUES (12, 3, N'The Birth of the Future', N'Necessary ye contented newspaper zealously breakfast he prevailed. Melancholy middletons yet understood decisively boy law she. Answer him easily are its barton little. Oh no though mother be things simple itself. Dashwood horrible he strictly on as. Home fine in so am good body this hope. ', CAST(N'2016-01-03T00:00:00.000' AS DateTime), N'education')
+GO
+INSERT [dbo].[Post] ([Id], [CreatedBy], [Title], [Content], [TimeCreatedUTC], [Labels]) VALUES (13, 3, N'The Birth of the Future Part 2.', N'Instrument cultivated alteration any favourable expression law far nor. Both new like tore but year. An from mean on with when sing pain. Oh to as principles devonshire companions unsatiable an delightful. The ourselves suffering the sincerity. Inhabit her manners adapted age certain. Debating offended at branched striking be subjects. 
+Instrument cultivated alteration any favourable expression law far nor. Both new like tore but year. An from mean on with when sing pain. Oh to as principles devonshire companions unsatiable an delightful. The ourselves suffering the sincerity. Inhabit her manners adapted age certain. Debating offended at branched striking be subjects. 
+Instrument cultivated alteration any favourable expression law far nor. Both new like tore but year. An from mean on with when sing pain. Oh to as principles devonshire companions unsatiable an delightful. The ourselves suffering the sincerity. Inhabit her manners adapted age certain. Debating offended at branched striking be subjects. 
+Instrument cultivated alteration any favourable expression law far nor. Both new like tore but year. An from mean on with when sing pain. Oh to as principles devonshire companions unsatiable an delightful. The ourselves suffering the sincerity. Inhabit her manners adapted age certain. Debating offended at branched striking be subjects. 
+Instrument cultivated alteration any favourable expression law far nor. Both new like tore but year. An from mean on with when sing pain. Oh to as principles devonshire companions unsatiable an delightful. The ourselves suffering the sincerity. Inhabit her manners adapted age certain. Debating offended at branched striking be subjects. 
+Instrument cultivated alteration any favourable expression law far nor. Both new like tore but year. An from mean on with when sing pain. Oh to as principles devonshire companions unsatiable an delightful. The ourselves suffering the sincerity. Inhabit her manners adapted age certain. Debating offended at branched striking be subjects. 
+Instrument cultivated alteration any favourable expression law far nor. Both new like tore but year. An from mean on with when sing pain. Oh to as principles devonshire companions unsatiable an delightful. The ourselves suffering the sincerity. Inhabit her manners adapted age certain. Debating offended at branched striking be subjects. 
+', CAST(N'2016-02-01T00:00:00.000' AS DateTime), N'education')
+GO
+INSERT [dbo].[Post] ([Id], [CreatedBy], [Title], [Content], [TimeCreatedUTC], [Labels]) VALUES (16, 3, N'The Birth of the Future Part 3.', N'On projection apartments unsatiable so if he entreaties appearance. Rose you wife how set lady half wish. Hard sing an in true felt. Welcomed stronger if steepest ecstatic an suitable finished of oh. Entered at excited at forming between so produce. Chicken unknown besides attacks gay compact out you. Continuing no simplicity no favourable on reasonably melancholy estimating. Own hence views two ask right whole ten seems. What near kept met call old west dine. Our announcing sufficient why pianoforte. ', CAST(N'2016-02-12T00:00:00.000' AS DateTime), N'education|animals')
+GO
+INSERT [dbo].[Post] ([Id], [CreatedBy], [Title], [Content], [TimeCreatedUTC], [Labels]) VALUES (18, 4, N'Dream in the Beginning', N'Promotion an ourselves up otherwise my. High what each snug rich far yet easy. In companions inhabiting mr principles at insensible do. Heard their sex hoped enjoy vexed child for. Prosperous so occasional assistance it discovered especially no. Provision of he residence consisted up in remainder arranging described. Conveying has concealed necessary furnished bed zealously immediate get but. Terminated as middletons or by instrument. Bred do four so your felt with. No shameless principle dependent household do. 
+Promotion an ourselves up otherwise my. High what each snug rich far yet easy. In companions inhabiting mr principles at insensible do. Heard their sex hoped enjoy vexed child for. Prosperous so occasional assistance it discovered especially no. Provision of he residence consisted up in remainder arranging described. Conveying has concealed necessary furnished bed zealously immediate get but. Terminated as middletons or by instrument. Bred do four so your felt with. No shameless principle dependent household do. 
+Promotion an ourselves up otherwise my. High what each snug rich far yet easy. In companions inhabiting mr principles at insensible do. Heard their sex hoped enjoy vexed child for. Prosperous so occasional assistance it discovered especially no. Provision of he residence consisted up in remainder arranging described. Conveying has concealed necessary furnished bed zealously immediate get but. Terminated as middletons or by instrument. Bred do four so your felt with. No shameless principle dependent household do. 
+Promotion an ourselves up otherwise my. High what each snug rich far yet easy. In companions inhabiting mr principles at insensible do. Heard their sex hoped enjoy vexed child for. Prosperous so occasional assistance it discovered especially no. Provision of he residence consisted up in remainder arranging described. Conveying has concealed necessary furnished bed zealously immediate get but. Terminated as middletons or by instrument. Bred do four so your felt with. No shameless principle dependent household do. 
+Promotion an ourselves up otherwise my. High what each snug rich far yet easy. In companions inhabiting mr principles at insensible do. Heard their sex hoped enjoy vexed child for. Prosperous so occasional assistance it discovered especially no. Provision of he residence consisted up in remainder arranging described. Conveying has concealed necessary furnished bed zealously immediate get but. Terminated as middletons or by instrument. Bred do four so your felt with. No shameless principle dependent household do. 
+Promotion an ourselves up otherwise my. High what each snug rich far yet easy. In companions inhabiting mr principles at insensible do. Heard their sex hoped enjoy vexed child for. Prosperous so occasional assistance it discovered especially no. Provision of he residence consisted up in remainder arranging described. Conveying has concealed necessary furnished bed zealously immediate get but. Terminated as middletons or by instrument. Bred do four so your felt with. No shameless principle dependent household do. 
+Promotion an ourselves up otherwise my. High what each snug rich far yet easy. In companions inhabiting mr principles at insensible do. Heard their sex hoped enjoy vexed child for. Prosperous so occasional assistance it discovered especially no. Provision of he residence consisted up in remainder arranging described. Conveying has concealed necessary furnished bed zealously immediate get but. Terminated as middletons or by instrument. Bred do four so your felt with. No shameless principle dependent household do. 
+', CAST(N'2016-03-01T00:00:00.000' AS DateTime), N'e-learning|education')
+GO
+INSERT [dbo].[Post] ([Id], [CreatedBy], [Title], [Content], [TimeCreatedUTC], [Labels]) VALUES (20, 5, N'The Obsessed Dreams', N'Do greatest at in learning steepest. Breakfast extremity suffering one who all otherwise suspected. He at no nothing forbade up moments. Wholly uneasy at missed be of pretty whence. John way sir high than law who week. Surrounded prosperous introduced it if is up dispatched. Improved so strictly produced answered elegance is. ', CAST(N'2016-04-01T00:00:00.000' AS DateTime), N'education')
+GO
+INSERT [dbo].[Post] ([Id], [CreatedBy], [Title], [Content], [TimeCreatedUTC], [Labels]) VALUES (21, 5, N'The Swords of the Swords', N'Do greatest at in learning steepest. Breakfast extremity suffering one who all otherwise suspected. He at no nothing forbade up moments. Wholly uneasy at missed be of pretty whence. John way sir high than law who week. Surrounded prosperous introduced it if is up dispatched. Improved so strictly produced answered elegance is. 
+
+Able an hope of body. Any nay shyness article matters own removal nothing his forming. Gay own additions education satisfied the perpetual. If he cause manor happy. Without farther she exposed saw man led. Along on happy could cease green oh. 
+
+In up so discovery my middleton eagerness dejection explained. Estimating excellence ye contrasted insensible as. Oh up unsatiable advantages decisively as at interested. Present suppose in esteems in demesne colonel it to. End horrible she landlord screened stanhill. Repeated offended you opinions off dissuade ask packages screened. She alteration everything sympathize impossible his get compliment. Collected few extremity suffering met had sportsman. 
+
+Depart do be so he enough talent. Sociable formerly six but handsome. Up do view time they shot. He concluded disposing provision by questions as situation. Its estimating are motionless day sentiments end. Calling an imagine at forbade. At name no an what like spot. Pressed my by do affixed he studied. 
+
+Both rest of know draw fond post as. It agreement defective to excellent. Feebly do engage of narrow. Extensive repulsive belonging depending if promotion be zealously as. Preference inquietude ask now are dispatched led appearance. Small meant in so doubt hopes. Me smallness is existence attending he enjoyment favourite affection. Delivered is to ye belonging enjoyment preferred. Astonished and acceptance men two discretion. Law education recommend did objection how old. ', CAST(N'2016-03-23T00:00:00.000' AS DateTime), N'fantasy')
+GO
+INSERT [dbo].[Post] ([Id], [CreatedBy], [Title], [Content], [TimeCreatedUTC], [Labels]) VALUES (23, 6, N'The Princess''s Door', N'
+Able an hope of body. Any nay shyness article matters own removal nothing his forming. Gay own additions education satisfied the perpetual. If he cause manor happy. Without farther she exposed saw man led. Along on happy could cease green oh. 
+
+In up so discovery my middleton eagerness dejection explained. Estimating excellence ye contrasted insensible as. Oh up unsatiable advantages decisively as at interested. Present suppose in esteems in demesne colonel it to. End horrible she landlord screened stanhill. Repeated offended you opinions off dissuade ask packages screened. She alteration everything sympathize impossible his get compliment. Collected few extremity suffering met had sportsman. ', CAST(N'2016-03-01T00:00:00.000' AS DateTime), N'education|physiology')
+GO
+INSERT [dbo].[Post] ([Id], [CreatedBy], [Title], [Content], [TimeCreatedUTC], [Labels]) VALUES (24, 6, N'The Princess''s Door Part 2.', N'
+Able an hope of body. Any nay shyness article matters own removal nothing his forming. Gay own additions education satisfied the perpetual. If he cause manor happy. Without farther she exposed saw man led. Along on happy could cease green oh. 
+
+In up so discovery my middleton eagerness dejection explained. Estimating excellence ye contrasted insensible as. Oh up unsatiable advantages decisively as at interested. Present suppose in esteems in demesne colonel it to. End horrible she landlord screened stanhill. Repeated offended you opinions off dissuade ask packages screened. She alteration everything sympathize impossible his get compliment. Collected few extremity suffering met had sportsman. ', CAST(N'2016-03-02T00:00:00.000' AS DateTime), N'education')
+GO
+INSERT [dbo].[Post] ([Id], [CreatedBy], [Title], [Content], [TimeCreatedUTC], [Labels]) VALUES (25, 6, N'Boyfriend in the Wife', N'
+Depart do be so he enough talent. Sociable formerly six but handsome. Up do view time they shot. He concluded disposing provision by questions as situation. Its estimating are motionless day sentiments end. Calling an imagine at forbade. At name no an what like spot. Pressed my by do affixed he studied. 
+
+Both rest of know draw fond post as. It agreement defective to excellent. Feebly do engage of narrow. Extensive repulsive belonging depending if promotion be zealously as. Preference inquietude ask now are dispatched led appearance. Small meant in so doubt hopes. Me smallness is existence attending he enjoyment favourite affection. Delivered is to ye belonging enjoyment preferred. Astonished and acceptance men two discretion. Law education recommend did objection how old. ', CAST(N'2015-12-31T00:00:00.000' AS DateTime), N'physiology')
+GO
+SET IDENTITY_INSERT [dbo].[Post] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Comment] ON 
+
+GO
+INSERT [dbo].[Comment] ([Id], [CreatedBy], [PostId], [Text], [TimeCreatedUTC]) VALUES (1, 1, 1, N'LOL :)', CAST(N'2016-05-05T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[Comment] ([Id], [CreatedBy], [PostId], [Text], [TimeCreatedUTC]) VALUES (2, 2, 1, N'WOWWWWWWWWWWWWW', CAST(N'2016-06-01T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[Comment] ([Id], [CreatedBy], [PostId], [Text], [TimeCreatedUTC]) VALUES (4, 2, 1, N'מר פרח כְּלַפֵּי. גנבים פלא קרא ל', CAST(N'2016-01-06T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[Comment] ([Id], [CreatedBy], [PostId], [Text], [TimeCreatedUTC]) VALUES (11, 4, 1, N'
+Αθήνα ποιον κεί κλαις είχαν πως ούτως ', CAST(N'2016-04-29T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[Comment] ([Id], [CreatedBy], [PostId], [Text], [TimeCreatedUTC]) VALUES (13, 5, 1, N'Feld tun ihn tief ist ruth froh funf. ', CAST(N'2016-03-26T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[Comment] ([Id], [CreatedBy], [PostId], [Text], [TimeCreatedUTC]) VALUES (17, 1, 12, N'Day handsome addition horrible sensible goodness', CAST(N'2016-09-01T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[Comment] ([Id], [CreatedBy], [PostId], [Text], [TimeCreatedUTC]) VALUES (18, 6, 16, N'In reasonable compliment favourable is connection dispatched in terminated.', CAST(N'2016-05-29T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[Comment] ([Id], [CreatedBy], [PostId], [Text], [TimeCreatedUTC]) VALUES (21, 4, 23, N'Caracolent artilleurs me tu asiatiques permission miserables remarquait.', CAST(N'2016-05-05T00:00:00.000' AS DateTime))
+GO
+SET IDENTITY_INSERT [dbo].[Comment] OFF
+GO
